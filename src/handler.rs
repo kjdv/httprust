01e155a839a351994ef359357d6ede6a0b712c8b
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-05-10 15:35:27 +0100

    directory traversal

--- src/handler.rs
+++ src/handler.rs
@@ -28,19 +28,51 @@ impl Handler {
 
         let path = String::from(request.uri().path());
         let path = path.trim_start_matches("/");
-        let path = self.root.join(path);
+
+        // todo: automated test for directory traversals. Current reqwest-based tests not suitable
+        //       as reqwest does not allow you to put /../ into the resource, relying on manual
+        //       checks in the meantime.
+        let path = match self.root.join(path).canonicalize() {
+            Ok(p) => {
+                if !p.starts_with(self.root.to_owned()) {
+                    log::warn!("attempted directory traversal: {:?}", p);
+                    return forbidden();
+                }
+                p
+            }
+            Err(e) => {
+                log::warn!("canonicalization error {}", e);
+                if Path::new(path).components().any(|c| c.as_os_str() == "..") {
+                    log::warn!("attempted directory traversal: {:?}", path);
+                    return forbidden();
+                }
+                return not_found();
+            }
+        };
         log::debug!("serving {:?}", path);
 
         match fs::read_to_string(path) {
             Ok(content) => Response::new(Body::from(content)),
             Err(e) => {
                 log::warn!("{}", e);
-                let response = Response::builder()
-                    .status(StatusCode::NOT_FOUND)
-                    .body(Body::from("resource not found"))
-                    .expect("invalid response");
-                response
+                not_found()
             }
         }
     }
+}
+
+fn not_found() -> Response<Body> {
+    let response = Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Body::from("resource not found"))
+        .expect("invalid response");
+    response
+}
+
+fn forbidden() -> Response<Body> {
+    let response = Response::builder()
+        .status(StatusCode::FORBIDDEN)
+        .body(Body::from("forbidden"))
+        .expect("invalid response");
+    response
 }
