6fc5cc8ebfb25f87bc74409e21814b8679204e02
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-05-12 05:30:48 +0100

    better path absolute

--- src/handler.rs
+++ src/handler.rs
@@ -1,16 +1,18 @@
+extern crate path_abs;
+
+use path_abs::PathDir;
 use super::log;
 use std::fs;
-use std::path::{Path, PathBuf};
 use hyper::{Body, Request, Response, StatusCode, Method};
 
 
 pub struct Handler {
-    root: PathBuf,
+    root: PathDir,
 }
 
 impl Handler {
     pub fn new(root: &str) -> std::io::Result<Handler> {
-        let root = Path::new(root).canonicalize()?;
+        let root = PathDir::new(root)?.canonicalize()?;
         log::info!("new handler for root at {:?}", root);
 
         Ok(Handler{
@@ -37,24 +39,19 @@ impl Handler {
 
         let path = String::from(request.uri().path());
         let path = path.trim_start_matches("/");
-
-        let path = match self.root.join(path).canonicalize() {
-            Ok(p) => {
-                if !p.starts_with(self.root.to_owned()) {
-                    log::warn!("attempted directory traversal: {:?}", p);
-                    return forbidden();
-                }
-                p
-            }
+        let path = match self.root.join(path).absolute() {
+            Ok(p) => p,
             Err(e) => {
-                log::warn!("canonicalization error {}", e);
-                if Path::new(path).components().any(|c| c.as_os_str() == "..") {
-                    log::warn!("attempted directory traversal: {:?}", path);
-                    return forbidden();
-                }
-                return not_found();
+                log::warn!("failed to absolute: {}", e);
+                return bad_request();
             }
         };
+
+        if !path.as_path().starts_with(self.root.to_owned()) {
+            log::warn!("attempted directory traversal: {:?}", path);
+            return forbidden();
+        }
+
         log::debug!("serving {:?}", path);
 
         match fs::read_to_string(path) {
@@ -94,6 +91,14 @@ fn method_not_allowed() -> Response<Body> {
     let response = Response::builder()
         .status(StatusCode::METHOD_NOT_ALLOWED)
         .body(Body::from("method not  allowed"))
+        .expect("invalid response");
+    response
+}
+
+fn bad_request() -> Response<Body> {
+    let response = Response::builder()
+        .status(StatusCode::BAD_REQUEST)
+        .body(Body::from("bad request"))
         .expect("invalid response");
     response
 }
